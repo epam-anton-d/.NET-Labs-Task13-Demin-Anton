871b7de484c1e9202fd431e8a9aac21461bca7c7
Anton D <Anton_Demin@epam.com>
2016-08-16 04:12:04 +0400

    commit 3

---
+++ Client/ConnectionFactory.cs
@@ -0,0 +1,51 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace Client.Core
+{
+    /// <summary>
+    /// Класс, соединяющий клиент с сервером через TCP-протокол.
+    /// </summary>
+    public class ConnectionFactory
+    {
+        // Указание, где ожидать входящие сообщения.
+        Uri address;
+
+        // Указание, как обмениваться сообщениями.
+        //BasicHttpBinding binding;
+        NetTcpBinding binding;
+
+        // Создание Конечной Точки.
+        EndpointAddress endpoint;
+
+        // Создание фабрики каналов.
+        ChannelFactory<IVirtualFilesystem> factory;
+
+        // Использование factory для создания канала (прокси).
+        public IVirtualFilesystem channel;
+
+
+        public ConnectionFactory()
+        {
+            // Указание, где ожидать входящие сообщения.
+            address = new Uri("net.tcp://127.0.0.1:4000/IVirtualFilesystem");
+
+            // Указание, как обмениваться сообщениями.
+            binding = new NetTcpBinding();
+
+            // Создание Конечной Точки.
+            endpoint = new EndpointAddress(address);
+
+            // Создание фабрики каналов.
+            factory = new ChannelFactory<IVirtualFilesystem>(binding, endpoint);
+
+            // Использование factory для создания канала (прокси).
+            channel = factory.CreateChannel();
+        }
+    }
+}
---
+++ Server/MessageDispatcher.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace Server.Core
+{
+    class MessageDispatcher
+    {
+        Uri address;
+        NetTcpBinding binding;
+        Type contract;
+        ServiceHost host;
+        
+        /// <summary>
+        /// Создает хост и ожидает пользовательские сообщения.
+        /// </summary>
+        public MessageDispatcher()
+        {
+            // Указание адреса, где ожидать входящие сообщения.
+            address = new Uri("net.tcp://127.0.0.1:4000/IVirtualFilesystem"); // ADDRESS.   (A)
+
+            // Указание привязки, как обмениваться сообщениями.
+            binding = new NetTcpBinding();                                   // BINDING.   (B)
+
+            // Указание контракта.
+            contract = typeof(IVirtualFilesystem);                        // CONTRACT.  (C) 
+
+            // Создание провайдера Хостинга с указанием Сервиса.
+            host = new ServiceHost(typeof(Filesystem));
+
+            // Добавление "Конечной Точки".
+            host.AddServiceEndpoint(contract, binding, address);
+
+            // Начало ожидания прихода сообщений.
+            host.Open();
+        }
+
+        ~MessageDispatcher()
+        {
+            // Завершение ожидания прихода сообщений.
+            host.Close();
+        }
+    }
+}
